294c208a8e48f29a84db2a407d62b8c5b983240b
Hossam Fekry <hossamfekryed@gmail.com>
2025-03-13 20:17:16 +0200

    delete the test files

--- test control brightness.py
+++
@@ -1,9 +0,0 @@
-import screen_brightness_control as sbc
-
-# Get current brightness
-current_brightness = sbc.get_brightness()
-print(f"Current Brightness: {current_brightness}%")
-
-# Set brightness to 50%
-sbc.set_brightness(100)
-print("Brightness set to 50%")
